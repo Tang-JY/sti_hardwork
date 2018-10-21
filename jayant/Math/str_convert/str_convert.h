820f7eae8e8651c56e0493af8dd0f3a07e1a2843
Jayant Tang <578900679@qq.com>
2018-10-21 22:05:52 +0800

    浮点数、整数转字符串

---
+++ jayant/Math/str_convert/str_convert.h
@@ -0,0 +1,13 @@
+#ifndef __STR_CONVERT_H_
+#define __STR_CONVERT_H_
+
+#include <stdint.h>
+
+
+int Int2Str(char* str, int data, int length);
+int Float2Str(char* str, float data, int precision, int length);
+
+
+
+
+#endif /* __STR_CONVERT_H_ */
